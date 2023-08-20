0978c865b6f233607b39c1838af0610ab8ee1e59
Jeron Windson <jeron_windson@hotmail.com>
2023-08-20 14:37:25 -0300

    commit inicial

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+const mode = document.getElementById('icon-mode');
+
+mode.addEventListener('click', () => {
+    const form = document.getElementById('form-login');
+    if(mode.classList.contains('fa-moon')) {
+        mode.classList.remove('fa-moon');
+        mode.classList.add('fa-sun');
+
+        form.classList.add('dark'); 
+        return;
+    }
+
+    mode.classList.add('fa-moon');
+    mode.classList.remove('fa-sun');
+    form.classList.remove('dark');
+
+});
+
